218ca8b6ff3006c80ab5b3833aaff302d8d7d9ea
Nico <nicodiazan@gmail.com>
2024-06-02 09:49:53 +0200

    Add basic condition checking to the bot

--- bot.js
+++ bot.js
@@ -6,11 +6,23 @@ class Bot {
 		this.patterns = pattern.patterns;
 	}
 
-    checkCondition(condition, message) {
+	checkPattern(pattern, message) {
+		const options = {
+			isCaseInsensitive: ~pattern.on.tags.indexOf("i"),
+		};
 
-    }
-    
+        return pattern.on.message.every((c) => this.checkMessageCondition(c, message, options))
+	}
 
+	checkMessageCondition(condition, message, options) {
+		if (typeof condition === "string") {
+			return message.includes(
+				options.isCaseInsensitive ? condition.toLowerCase() : condition
+			);
+		}
+
+		return condition.some((c) => message.includes(c));
+	}
 }
 
 module.exports = Bot;
